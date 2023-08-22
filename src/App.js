4fd8ee542fb0332bcae5eb88eef57074eb7458d4
changach-3000 <sharon.changach@student.moringaschool.com>
2023-08-22 22:01:33 +0300

    add a layout page and add routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import LayoutPage from "./pages/LayoutPage";
 
 function App() {
   return (
     <div className="App">
-     <LandingPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LayoutPage/>}>
+          <Route index element={<LandingPage/>}/>
+          
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
